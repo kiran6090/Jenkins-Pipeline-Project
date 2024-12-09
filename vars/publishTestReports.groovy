7ac3dddbe1820617ad43949cb9a72f96293e8bc0
Shankar Mandal Kiran (SDS/EVS3) <hzi2kor@bosch.com>
2024-12-09 21:11:44 +0530

    31th commit

--- vars/publishTestReports.groovy
+++ vars/publishTestReports.groovy
@@ -1,17 +1,16 @@
 def call() {
     stage('Publish Reports') {
-        steps {
-            echo 'Publishing test reports...'
-            // Adjust the paths and tools based on your test framework and report type
-            publishHTML(target: [
-                allowMissing: false,
-                alwaysLinkToLastBuild: true,
-                keepAll: true,
-                reportDir: 'test-reports/html',
-                reportFiles: 'index.html',
-                reportName: 'Test Report'
-            ])
-        }
+        echo 'Publishing test reports...'
+        // Use publishHTML to publish HTML reports
+        publishHTML(target: [
+            allowMissing: false,
+            alwaysLinkToLastBuild: true,
+            keepAll: true,
+            reportDir: 'test-reports/html',
+            reportFiles: 'index.html',
+            reportName: 'Test Report'
+        ])
     }
 }
+
 
--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -1,8 +1,10 @@
-// vars/runTests.groovy
 def call() {
     // Step 1: Run the specific pytest test file 'test/test_api.py'
     echo 'Running pytest tests for test/test_api.py...'
-    bat 'pytest test --maxfail=1 --disable-warnings -q '
+    bat '''
+        pytest test/test_api.py --html=test-reports/html/index.html --self-contained-html --maxfail=1 --disable-warnings -q
+    '''
 }
+
 
 
