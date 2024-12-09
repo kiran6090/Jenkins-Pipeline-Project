3c1a3d1656045f2b9f70e3bdad52d913ec45f6c7
Shankar Mandal Kiran (SDS/EVS3) <hzi2kor@bosch.com>
2024-12-09 16:14:49 +0530

    19th commit

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -1,6 +1,8 @@
+// vars/runTests.groovy
 def call() {
-    // Step 1: Run tests using pytest
-    echo 'Running pytest tests...'
-    bat 'pytest test/ --maxfail=1 --disable-warnings -q'
+    // Step 1: Run the specific pytest test file 'test/test_api.py'
+    echo 'Running pytest tests for test/test_api.py...'
+    bat 'pytest test/test_api.py --maxfail=1 --disable-warnings -q'
 }
+
 
