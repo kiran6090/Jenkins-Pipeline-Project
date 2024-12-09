4485a4663cc0e9d9b21eafe4250bb1c6cbf6316a
Shankar Mandal Kiran (SDS/EVS3) <hzi2kor@bosch.com>
2024-12-09 22:19:09 +0530

    34th commit

--- vars/publishTestReports.groovy
+++ vars/publishTestReports.groovy
@@ -1,9 +1,7 @@
 def call() {
-    // Publish JUnit test results
-    junit '**/reports/test-results.xml'
-
-    // Archive the reports for later use
-    archiveArtifacts artifacts: 'reports/test-results.xml', allowEmptyArchive: true
+    // Step 2: Generate the test report
+    echo 'Generating test report...'
+    bat 'pytest --junitxml=reports/test-results.xml'
 }
 
 
