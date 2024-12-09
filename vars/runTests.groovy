609a7b5ab9b170dfb7918aac194e0eed0d9d545d
Shankar Mandal Kiran (SDS/EVS3) <hzi2kor@bosch.com>
2024-12-09 19:38:00 +0530

    26 commit

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -1,18 +1,8 @@
 // vars/runTests.groovy
 def call() {
-    // Step 1: Create and activate the virtual environment, install pytest
-    echo 'Creating and activating virtual environment...'
-    bat '''
-        python -m venv venv
-        .\\venv\\Scripts\\activate
-        pip install pytest
-        pip show pytest  REM Check if pytest is installed successfully
-    '''
-    
-    // Step 2: Run the specific pytest test file 'test/test_api.py' in the same shell session
+    // Step 1: Run the specific pytest test file 'test/test_api.py'
     echo 'Running pytest tests for test/test_api.py...'
-    bat '''
-        .\\venv\\Scripts\\activate && pytest --version && pytest test/test_api.py --maxfail=1 --disable-warnings -q
-    '''
+    bat 'pytest test --maxfail=1 --disable-warnings -q'
 }
+
 
